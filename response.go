ac75a364ea99b8315cb1a5a5553b2ff7ae81a938
Daniel Cosme <cosmedaniel8@gmail.com>
2023-04-09 18:56:17 -0400

    Implement RepsonseWriter interface

--- response.go
+++ response.go
@@ -14,6 +14,14 @@ func NewResponseWriterPlus(rw http.ResponseWriter) *ResponseWriterPlus {
 	return &ResponseWriterPlus{ResponseWriter: rw}
 }
 
+func (rw *ResponseWriterPlus) Header() http.Header {
+	return rw.Header()
+}
+
+func (rw *ResponseWriterPlus) Write(b []byte) (int, error) {
+	return rw.Write(b)
+}
+
 func (rw *ResponseWriterPlus) WriteHeader(code int) {
 	rw.status = code
 	rw.ResponseWriter.WriteHeader(code)
